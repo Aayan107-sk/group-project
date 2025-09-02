da67bc00c4c001810d42f8fc6c5709d204a92140
aayan <aayan.777107@gmail.com>
2025-09-02 10:34:33 +0530

    add login file and context page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from './logo'
 const Navbar = () => {
   return (
     <div className='h-screen  fixed top-0 left-0 right-0 w-[15%] bg-[#F1F4F9]' >
-      <div className="h-28 bg-red-500 flex">
+      <div className="h-28 text-2xl bg-red-500 flex">
         <Logo color="blue" size={20} />
         <h1>AI Resume Analyzer</h1></div>
       
---
+++ src/contexts/UesrContext.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UesrContext = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UesrContext
