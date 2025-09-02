b61ffcb7b8b6e5f5c8ba5e1b06c0179b28315a77
siddiqui sameer <siddiquisamer98@gmail.com>
2025-09-02 14:29:53 +0530

    asdfghjk

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Navbar from './components/Navbar'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Login />
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+    <>
+      <div className='min-h-screen flex items-center justify-center bg-gray-50'>
+        <div className='max-w-4xl w-full bg-white rounded-2xl shadow-md grid grid-cols-1 md:grid-cols-2 gap-6 px-6 py-8'>
+
+          <div className='flex flex-col justify-center'>
+            <div className='flex items-center space-x-3 mb-8'>
+              <div className='bg-indigo-500 text-white p-3 rounded-xl'>
+                <span className='material-icon'>smart_toy</span>
+              </div>
+              <div>
+                <h1 className='text-lg font-semibold'>AI Resume Analyzer</h1>
+                <p className='text-sm text-gray-500'>Smart Hiring with AI</p>
+              </div>
+            </div>
+            <h1 className='text-2xl font'>Welcome back</h1>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+    </>
   )
 }
 
