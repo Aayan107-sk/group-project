0c348ce310a53068be8492b3795fd51782a2ee89
aayan <aayan.777107@gmail.com>
2025-09-04 14:44:37 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Login  from './pages/Login'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -9,16 +9,21 @@ import ResumeList from './pages/ResumeList';
 import ResumesPage from './pages/ResumeList';
 
 const App = () => {
+  const [login,setLogin]=useState(false);
   return (
     <div>
+  {login?
     <Layout>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/resumelist" element={<ResumesPage/>}/>
       </Routes>
     </Layout>
-    
+    :
+     <Routes>
+        <Route path="/login" element={<Login />} /> 
+      </Routes>
+    }
     
     </div>
   )
